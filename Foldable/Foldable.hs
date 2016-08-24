1c63c66c6f3ce388b67b8b0c409cc823fa8846b9
Jiao Xiang <jxwr.cn@gmail.com>
2016-08-25 02:10:29 +0800

    add foldMap impl to Foldable example

--- Foldable/Foldable.hs
+++ Foldable/Foldable.hs
@@ -1,3 +1,4 @@
+import Data.Monoid
 import Data.Foldable (
   foldr,
   toList,
@@ -6,6 +7,7 @@ import Data.Foldable (
   mapM_,
   length
   )
+
 
 -- ;; traverse_ :: (Foldable t, Applicative f) => (a -> f b) -> t a -> f ()
 -- ;; mapM_     :: (Foldable t, Monad m) => (a -> m b) -> t a -> m ()
@@ -18,7 +20,7 @@ instance Foldable List where
   foldr f b Nil = b
   foldr f b (Cons a xs) = f a (foldr f b xs)
 
-main = do
+test_foldr_impl = do
   let xs = (Cons 1 (Cons 2 (Cons 3 Nil)))
   
   print $ foldr (+) 0 xs
@@ -32,3 +34,40 @@ main = do
   mapM_ print xs >> putStrLn "-"
   -- 依次执行Action组成的Foldable
   sequence_ (Cons (putStrLn "a ") (Cons (putStrLn "b") Nil))
+  
+
+data List' a = Nil' | Cons' a (List' a)
+
+instance Monoid (List' a) where
+  -- mempty :: a
+  mempty = Nil'
+  -- mappend :: a -> a -> a
+  Nil' `mappend` ys = ys
+  (Cons' a xs) `mappend` ys = Cons' a (xs `mappend` ys)
+
+instance Foldable List' where
+  -- foldMap :: Monoid m => (a -> m) -> t a -> m
+  foldMap f Nil' = mempty
+  foldMap f (Cons' a xs) = f a `mappend` foldMap f xs
+
+test_foldMap_impl = do
+  let xs' = (Cons' 1 (Cons' 2 (Cons' 3 Nil')))
+  
+  print $ foldr (+) 0 xs'
+  print $ toList xs'
+  print $ length xs'
+  print (sum xs') >> putStrLn "-"
+
+  -- 遍历Foldable，执行某个Action
+  traverse_ print xs' >> putStrLn "-"
+  -- 跟traverse_一样，区别在于返回类型是Applicative还是Monad，就是说traverse_总是可用
+  mapM_ print xs' >> putStrLn "-"
+  -- 依次执行Action组成的Foldable
+  sequence_ (Cons' (putStrLn "a ") (Cons' (putStrLn "b") Nil'))    
+
+main = do
+  putStrLn "test_foldr_impl:" >> test_foldr_impl
+  putStrLn "test_foldMap_impl:" >> test_foldMap_impl
+
+
+  
