c809e27e449c497b4f69f4543d6369a3c6622074
Jiao Xiang <jxwr.cn@gmail.com>
2016-08-25 01:49:58 +0800

    add Foldable.hs

---
+++ Foldable/Foldable.hs
@@ -0,0 +1,34 @@
+import Data.Foldable (
+  foldr,
+  toList,
+  sequence_,
+  traverse_,
+  mapM_,
+  length
+  )
+
+-- ;; traverse_ :: (Foldable t, Applicative f) => (a -> f b) -> t a -> f ()
+-- ;; mapM_     :: (Foldable t, Monad m) => (a -> m b) -> t a -> m ()
+-- ;; sequence_ :: (Foldable t, Monad m) => t (m a) -> m ()
+
+data List a = Nil | Cons a (List a)
+
+instance Foldable List where
+  -- foldr :: (a -> b -> b) -> b -> List a -> b
+  foldr f b Nil = b
+  foldr f b (Cons a xs) = f a (foldr f b xs)
+
+main = do
+  let xs = (Cons 1 (Cons 2 (Cons 3 Nil)))
+  
+  print $ foldr (+) 0 xs
+  print $ toList xs
+  print $ length xs
+  print (sum xs) >> putStrLn "-"
+
+  -- 遍历Foldable，执行某个Action
+  traverse_ print xs >> putStrLn "-"
+  -- 跟traverse_一样，区别在于返回类型是Applicative还是Monad，就是说traverse_总是可用
+  mapM_ print xs >> putStrLn "-"
+  -- 依次执行Action组成的Foldable
+  sequence_ (Cons (putStrLn "a ") (Cons (putStrLn "b") Nil))
