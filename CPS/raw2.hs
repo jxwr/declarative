2eb45c2d26a6c774cc1b72023160c13c13151f26
Jiao Xiang <jxwr.cn@gmail.com>
2016-09-03 09:57:06 +0800

    add raw2.hs

---
+++ CPS/raw2.hs
@@ -0,0 +1,44 @@
+
+addap :: (Int -> Int) -> Int -> Int
+addap f x = x + (f x)
+
+addap_cps :: (Int -> ((Int -> r) -> r)) -> Int -> (Int -> r) -> r
+addap_cps f_cps x k = f_cps x $ \fx -> k $ x + fx
+
+index :: Eq a => [a] -> a -> Int
+index [] _ = -1
+index (x:xs) a
+  | x == a = 0
+  | otherwise =
+      let n = index xs a
+      in if n >= 0
+         then n + 1
+         else -1
+
+index_cps :: Eq a => [a] -> a -> (Int -> r) -> r
+index_cps [] _ k = k (-1 :: Int)
+index_cps (x:xs) a k
+  | x == a = k 0
+  | otherwise =
+      let n = index xs a
+      in if n >= 0
+         then k (n + 1)
+         else k (-1 :: Int)
+
+main = do
+  addap_cps (\x -> (\k -> k (x * 2))) 10 print
+  print $ addap_cps (\x -> (\k -> k (x * 2))) 10 (+ 2)
+
+  putStrLn "----"
+  
+  print $ index [1,2,3,4] 3
+  print $ index [] 3
+  print $ index [1,2] 3
+  print $ index [3,1] 3
+  print $ index [1,3] 3
+  print $ map (index ['a'..'z']) ['a'..'z']
+
+  putStrLn "----"
+
+  index_cps [1,2,3,4] 3 print
+  mapM_ (\x -> index_cps ['a'..'z'] x print) ['a'..'z']
